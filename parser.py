eb74764bc790c069504f34ed9482f74a22a11c17
Paul Gaudin <paulgaudin314@gmail.com>
2020-06-25 13:35:42 -0700

    back to for loop for final array

--- parser.py
+++ parser.py
@@ -83,7 +83,6 @@ def load_annotations(data_folder):
         #     print("1M")
 
     # print(final_array[-1])
-    return final_array
-    # for entry in final_array:
-    #     yield entry
-    
+    # return final_array
+    for entry in final_array:
+        yield entry
