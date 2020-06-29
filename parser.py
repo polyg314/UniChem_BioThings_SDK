cfe0136ce5c910407862f330336638790ea325fb
Paul Gaudin <paulgaudin314@gmail.com>
2020-06-29 10:54:32 -0700

    added bigger test files

--- parser.py
+++ parser.py
@@ -9,8 +9,8 @@ logging = config.logger
 def load_annotations(data_folder):
     # load source files
     source_file = os.path.join(data_folder,"UC_SOURCE.txt")
-    struct_file = os.path.join(data_folder,"UC_STRUCTURE.txt")
-    xref_file = os.path.join(data_folder,"UC_XREF.txt")
+    struct_file = os.path.join(data_folder,"UC_SP2.txt")
+    xref_file = os.path.join(data_folder,"UC_XP2.txt")
     assert os.path.exists(source_file)
     assert os.path.exists(struct_file)
     assert os.path.exists(xref_file)
