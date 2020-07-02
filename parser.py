ee390159d4e6ccf3635cdf3ae71b6c45f868985e
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-02 13:53:55 -0700

    integrated sorting and if statement to determine if ucis were in range of certain chunks

--- parser.py
+++ parser.py
@@ -14,11 +14,11 @@ logging = config.logger
 
 def load_annotations(data_folder):
 
-    current_chunk_size = 1000000;
+    current_chunk_size = 100000;
     # load source files
     source_file = os.path.join(data_folder,"UC_SOURCE.txt")
-    struct_file = os.path.join(data_folder,"UC_STRUCTURE.txt")
-    xref_file = os.path.join(data_folder,"UC_XREF.txt")
+    struct_file = os.path.join(data_folder,"UC_SP900.txt")
+    xref_file = os.path.join(data_folder,"UC_XP900.txt")
     assert os.path.exists(source_file)
     assert os.path.exists(struct_file)
     assert os.path.exists(xref_file)
@@ -30,33 +30,84 @@ def load_annotations(data_folder):
     source_dict = {source_keys[i]: source_values[i] for i in range(len(source_keys))}     
 
     
-    xdtype={'uci':'int32','src_id':'int8','src_compound_id':'str'}
+    ## make structure file 
+
+    # read through file in chunks - too big to loadd all at once
+    sdtype={'uci':'int64','standardinchikey':'str'}
     
-    xref_df_chunk = pd.read_csv(xref_file, sep='\t', header=None, usecols=['uci','src_id','src_compound_id'],
+    structure_df_chunk = pd.read_csv(struct_file, sep='\t', header=None, usecols=['uci', 'standardinchikey'],
+                                         names=['uci_old','standardinchi','standardinchikey','created','username','fikhb','uci','parent_smiles'],
+                                         chunksize=current_chunk_size, dtype=sdtype) 
+
+    
+    # append structure chunks to list
+    smerge_counter = 0;
+
+    for chunk in structure_df_chunk:
+        if(smerge_counter == 0):
+            chunk.to_csv(path_or_buf=os.path.join(data_folder,"structure_df.csv"), index=False)
+            smerge_counter = 1;  
+        else:
+            chunk.to_csv(path_or_buf=os.path.join(data_folder,"structure_df.csv"), index=False, mode='a', header=False)    
+
+    del structure_df_chunk
+
+    csvsort(os.path.join(data_folder,"structure_df.csv"),[1],numeric_column=True)
+
+    ## make xref file   
+    
+    xdtype={'src_id':'int8','src_compound_id':'str','uci':'int64'}
+    
+    xref_df_chunk = pd.read_csv(xref_file, sep='\t', header=None, usecols=['src_id','src_compound_id', 'uci'],
                                          names=['uci_old','src_id','src_compound_id','assignment','last_release_u_when_current','created ','lastupdated','userstamp','aux_src','uci'],
                                          chunksize=current_chunk_size, dtype=xdtype)  
 
-    sdtype={'uci':'int32','standardinchikey':'str'}
+    xmerge_counter = 0;
+
+    for chunk in xref_df_chunk:
+        if(xmerge_counter == 0):
+            chunk.to_csv(path_or_buf=os.path.join(data_folder,"xref_df.csv"), index=False)
+            xmerge_counter = 1;  
+        else:
+            chunk.to_csv(path_or_buf=os.path.join(data_folder,"xref_df.csv"), index=False, mode='a', header=False)    
+
+
+    csvsort(os.path.join(data_folder,"xref_df.csv"),[2],numeric_column=True)
+
+    chunk_counter = 0;
+    
+    structure_df_chunk = pd.read_csv(os.path.join(data_folder,"structure_df.csv"), chunksize=current_chunk_size)
+    min_max_columns = ["chunk_start", "min_uci", "max_uci"];
+    structure_min_max_df = pd.DataFrame(columns = min_max_columns)
+    for schunk in structure_df_chunk:
+        chunk_start = chunk_counter*current_chunk_size
+        chunk_min = min(schunk["uci"])
+        chunk_max = max(schunk["uci"])
+        chunk_counter = chunk_counter + 1;
+        structure_min_max_df = structure_min_max_df.append(pd.DataFrame([[chunk_start,chunk_min,chunk_max]], columns = min_max_columns))
+    
+    xdf_chunk = pd.read_csv(os.path.join(data_folder,"xref_df.csv"), chunksize=current_chunk_size) 
 
     merge_counter = 0; 
+    for xchunk in xdf_chunk:
+        current_x_min = min(xchunk["uci"])
+        current_x_max = max(xchunk["uci"])
+        for index, row in structure_min_max_df.iterrows():
+            if(not((current_x_max < row['min_uci']) or (current_x_min > row['max_uci']))):
+                sdf_chunk = pd.read_csv(os.path.join(data_folder,"structure_df.csv"), skiprows = row["chunk_start"], header=0, names=['standardinchikey','uci'], nrows=current_chunk_size)
+                complete_df_chunk = pd.merge(left=sdf_chunk, right=xchunk, left_on='uci', right_on='uci')
+                if(merge_counter == 0):
+                    complete_df_chunk.to_csv(path_or_buf=os.path.join(data_folder,"complete_df.csv"), index=False)
+                    merge_counter = 1;  
+                else:
+                    complete_df_chunk.to_csv(path_or_buf=os.path.join(data_folder,"complete_df.csv"), index=False, mode='a', header=False)
+        
+        
+    del sdf_chunk
+    del xdf_chunk 
 
-    for xchunk in xref_df_chunk:
-        structure_df_chunk = pd.read_csv(struct_file, sep='\t', header=None, usecols=['uci', 'standardinchikey'],
-                                         names=['uci_old','standardinchi','standardinchikey','created','username','fikhb','uci','parent_smiles'],
-                                         chunksize=current_chunk_size, dtype=sdtype) 
-        for schunk in structure_df_chunk:
-            complete_df_chunk = pd.merge(left=schunk, right=xchunk, left_on='uci', right_on='uci')
-            if(merge_counter == 0):
-                complete_df_chunk.to_csv(path_or_buf=os.path.join(data_folder,"complete_df.csv"), index=False)
-                merge_counter = 1;  
-            else:
-                complete_df_chunk.to_csv(path_or_buf=os.path.join(data_folder,"complete_df.csv"), index=False, mode='a', header=False)  
-
-
-    del structure_df_chunk
-    del xref_df_chunk 
-
-    csvsort(os.path.join(data_folder,"complete_df.csv"),[0])
+    
+    csvsort(os.path.join(data_folder,"complete_df.csv"),[0], numeric_column = False)
 
     new_entry = {}
     last_inchi = '';
