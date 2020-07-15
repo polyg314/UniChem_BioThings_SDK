006e77c6bc9db2433c8e160d1e860ee7989dd84e
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-15 15:55:06 -0700

    dump file and init file

--- dump.py
+++ dump.py
@@ -1,3 +1,10 @@
+import os
+import biothings, config
+biothings.config_for_app(config)
+from config import DATA_ARCHIVE_ROOT
+from biothings.utils.common import uncompressall
+import biothings.hub.dataload.dumper
+
 import sys
 from .ftplib import ftplib
 
@@ -40,3 +47,19 @@ def get_latest_unichem_urls():
 
 	return([source, structure, xref])
 
+
+
+class UniChemDumper(biothings.hub.dataload.dumper.LastModifiedHTTPDumper):
+
+    SRC_NAME = "unichem"
+    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
+    SCHEDULE = None
+    UNCOMPRESS = True
+    SRC_URLS = get_latest_unichem_urls()
+    __metadata__ = {"src_meta": {}}
+
+    def post_dump(self, *args, **kwargs):
+        if self.__class__.UNCOMPRESS:
+            self.logger.info("Uncompress all archive files in '%s'" %
+                             self.new_data_folder)
+            uncompressall(self.new_data_folder)
