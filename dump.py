71f2abbeb21f52a8967d8d6e5c622960dfc1d236
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-17 12:06:04 -0700

    create dump list, source only

--- dump.py
+++ dump.py
@@ -16,19 +16,23 @@ from biothings.hub.dataload.dumper import FTPDumper, DumperException
 class Unichem_biothings_sdkDumper(FTPDumper):
 
     SRC_NAME = "UniChem_BioThings_SDK"
-    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
-    SCHEDULE = None
+    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)    
+    # SCHEDULE = None
     UNCOMPRESS = True
-    SRC_URLS = [
-        'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',
-        'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_STRUCTURE.txt.gz',
-        'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_XREF.txt.gz'
-    ]
+    # SRC_URLS = [
+    #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',
+    #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_STRUCTURE.txt.gz',
+    #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_XREF.txt.gz'
+    # ]
     # self.SRC_URLS = [
     #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',
     #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_STRUCTURE.txt.gz',
     #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_XREF.txt.gz'
     # ]
+
+    def create_todump_list(self):
+    	source_local = os.path.join(self.new_data_folder,"UC_SOURCE.txt.gz")
+    	self.to_dump.append({"remote": 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',"local":local})
 
     __metadata__ = {"src_meta": {}}
 
