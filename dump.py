5497ca48a1d9f2679f84fed6fce9c498ea489bb4
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-16 20:06:00 -0700

    meh

--- dump.py
+++ dump.py
@@ -12,20 +12,20 @@ import sys
 from .ftplib import *
 
 
-
-
 class Unichem_biothings_sdkDumper(biothings.hub.dataload.dumper.LastModifiedFTPDumper):
 
     SRC_NAME = "UniChem_BioThings_SDK"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
     SCHEDULE = None
     UNCOMPRESS = True
-    SRC_URLS = [
-        'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',
-        'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_STRUCTURE.txt.gz',
-        'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_XREF.txt.gz'
-    ]
+    # SRC_URLS = [
+    #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',
+    #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_STRUCTURE.txt.gz',
+    #     'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_XREF.txt.gz'
+    # ]
     __metadata__ = {"src_meta": {}}
+
+    self.download("ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz", os.path.join(DATA_ARCHIVE_ROOT, "UC_SOURCE.txt.gz"), headers={})
 
     def post_dump(self, *args, **kwargs):
         if self.__class__.UNCOMPRESS:
