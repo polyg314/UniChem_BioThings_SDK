7e6293abeaa5e211d089ba8bfe9de6c69c1861ca
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-17 12:36:08 -0700

    set local source

--- dump.py
+++ dump.py
@@ -74,7 +74,7 @@ class Unichem_biothings_sdkDumper(FTPDumper):
     	# self.get_release()
     	self.release = 'UDRI283'
     	source_local = os.path.join(self.new_data_folder,"UC_SOURCE.txt.gz")
-    	self.to_dump.append({"remote": 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',"local":local})
+    	self.to_dump.append({"remote": 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',"local":source_local})
 
     # __metadata__ = {"src_meta": {}}
 
