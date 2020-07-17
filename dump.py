5670abae8211cf17f7f9a6f56e6c1060e8ddb722
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-17 12:29:31 -0700

    simplify again

--- dump.py
+++ dump.py
@@ -34,22 +34,48 @@ class Unichem_biothings_sdkDumper(FTPDumper):
     VERSION_DIR = '/pub/databases/chembl/UniChem/data/oracleDumps'
 
 
-    def get_release(self):
-        try:
-            self.client.cwd(self.__class__.VERSION_DIR)
-            releases = sorted(self.client.nlst())
-            if len(releases) == 0:
-                raise DumperException("Can't any release information in '%s'" % self.__class__.VERSION_DIR)
-            self.release = releases[-1]
-        finally:
-            self.client.cwd(self.__class__.CWD_DIR)
+    # def get_release(self):
+    #     try:
+    #         self.client.cwd(self.__class__.VERSION_DIR)
+    #         releases = sorted(self.client.nlst())
+    #         if len(releases) == 0:
+    #             raise DumperException("Can't any release information in '%s'" % self.__class__.VERSION_DIR)
+    #         self.release = releases[-1]
+    #     finally:
+    #         self.client.cwd(self.__class__.CWD_DIR)
+
+    # def new_release_available(self):
+    #     current_release = self.src_doc.get("download",{}).get("release")
+    #     if not current_release or self.release > current_release:
+    #         self.logger.info("New release '%s' found" % self.release)
+    #         return True
+    #     else:
+    #         self.logger.debug("No new release found")
+    #         return False
+
+    # def create_todump_list(self, force=False):
+    #     self.get_release()
+    #     if force or self.new_release_available():
+    #         # get list of files to download
+    #         remote_files = self.client.nlst()
+    #         for remote in remote_files:
+    #             try:
+    #                 local = os.path.join(self.new_data_folder,remote)
+    #                 if not os.path.exists(local) or self.remote_is_better(remote,local):
+    #                     self.to_dump.append({"remote": remote,"local":local})
+    #             except ftplib.error_temp as e:
+    #                 self.logger.debug("Recycling FTP client because: '%s'" % e)
+    #                 self.release_client()
+    #                 self.prepare_client()
+
+                    
 
     def create_todump_list(self, force=False):
     	self.get_release()
     	source_local = os.path.join(self.new_data_folder,"UC_SOURCE.txt.gz")
     	self.to_dump.append({"remote": 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',"local":local})
 
-    __metadata__ = {"src_meta": {}}
+    # __metadata__ = {"src_meta": {}}
 
     def post_dump(self, *args, **kwargs):
         if self.__class__.UNCOMPRESS:
