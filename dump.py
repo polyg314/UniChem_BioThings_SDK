f237c05465c8ec57878ca7dad262832532519277
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-17 13:51:57 -0700

    ftp login

--- dump.py
+++ dump.py
@@ -52,7 +52,7 @@ class Unichem_biothings_sdkDumper(FTPDumper):
         self.get_newest_info()
        	ftp = ftplib.FTP("ftp.ebi.ac.uk")
 		# #login
-		# ftp.login()
+		ftp.login()
         for fn in ["UC_SOURCE.txt.gz"]:
             local_file = os.path.join(self.new_data_folder,fn)
             if force or not os.path.exists(local_file) or self.remote_is_better(fn,local_file) or self.new_release_available():
