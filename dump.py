40a38a49c9b2a4c0e1cae037450e26a0cb9dc977
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-17 12:31:07 -0700

    again

--- dump.py
+++ dump.py
@@ -71,7 +71,7 @@ class Unichem_biothings_sdkDumper(FTPDumper):
                     
 
     def create_todump_list(self, force=False):
-    	self.get_release()
+    	# self.get_release()
     	source_local = os.path.join(self.new_data_folder,"UC_SOURCE.txt.gz")
     	self.to_dump.append({"remote": 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/UDRI283/UC_SOURCE.txt.gz',"local":local})
 
