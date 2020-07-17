8e85e38a934a746bbd048db557f9c17f898b19fe
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-16 19:23:30 -0700

    messy

--- dump.py
+++ dump.py
@@ -17,36 +17,36 @@ from .ftplib import *
 
 class Unichem_biothings_sdkDumper(biothings.hub.dataload.dumper.LastModifiedFTPDumper):
 
-	ftp = ftplib.FTP("ftp.ebi.ac.uk")
+	# ftp = ftplib.FTP("ftp.ebi.ac.uk")
 
-	# login
-	ftp.login()
+	# # login
+	# ftp.login()
 
-	# move to the target directory
-	ftp.cwd('/pub/databases/chembl/UniChem/data/oracleDumps')
+	# # move to the target directory
+	# ftp.cwd('/pub/databases/chembl/UniChem/data/oracleDumps')
 
-	# get the directory listing
-	files: list = ftp.nlst()
+	# # get the directory listing
+	# files: list = ftp.nlst()
 
-	# close the ftp connection
-	ftp.quit()
+	# # close the ftp connection
+	# ftp.quit()
 
-	# init the starting point
-	target_dir_index = 0
+	# # init the starting point
+	# target_dir_index = 0
 
-	# parse the list to determine the latest version of the files
-	for f in files:
-		# is this file greater that the previous
-		if "UDRI" in f:
-			# convert the suffix into an int and compare it to the previous one
-			if int(f[4:]) > target_dir_index:
-				# save this as our new highest value
-				target_dir_index = int(f[4:])
+	# # parse the list to determine the latest version of the files
+	# for f in files:
+	# 	# is this file greater that the previous
+	# 	if "UDRI" in f:
+	# 		# convert the suffix into an int and compare it to the previous one
+	# 		if int(f[4:]) > target_dir_index:
+	# 			# save this as our new highest value
+	# 			target_dir_index = int(f[4:])
 
-	# make urls for source, structure, and xref files 
-	source = 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/' + f + '/UC_SOURCE.txt.gz';
-	structure = 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/' + f + '/UC_STRUCTURE.txt.gz';
-	xref = 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/' + f + '/UC_XREF.txt.gz';
+	# # make urls for source, structure, and xref files 
+	# source = 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/' + f + '/UC_SOURCE.txt.gz';
+	# structure = 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/' + f + '/UC_STRUCTURE.txt.gz';
+	# xref = 'ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/' + f + '/UC_XREF.txt.gz';
 		
     SRC_NAME = "UniChem_BioThings_SDK"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
@@ -58,7 +58,7 @@ class Unichem_biothings_sdkDumper(biothings.hub.dataload.dumper.LastModifiedFTPD
 
     #SCHEDULE = "0 12 * * *"
 
-    __metadata__ = {"src_meta": {}}
+    # __metadata__ = {"src_meta": {}}
 
     def get_latest_unichem_urls():
 		"""Function largely borrowed from 
