c163b10f423f8fa2daa79c7f72679b357bb07082
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-17 16:59:45 -0700

    Uncompress

--- dump.py
+++ dump.py
@@ -26,7 +26,7 @@ class Unichem_biothings_sdkDumper(FTPDumper):
     SRC_NAME = "UniChem_BioThings_SDK"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)  
 
-	# UNCOMPRESS = True
+	# 
     FTP_HOST = 'ftp.ebi.ac.uk'
     CWD_DIR = '/pub/databases/chembl/UniChem/data/oracleDumps'
     FTP_USER = ''
@@ -34,6 +34,7 @@ class Unichem_biothings_sdkDumper(FTPDumper):
     FTP_TIMEOUT = 10 * 60.0  # we want dumper to timout if necessary
     # ARCHIVE = False
     #SCHEDULE = "0 6 * * *"
+    UNCOMPRESS = True
 
     def get_newest_info(self):
         self.client.cwd("/pub/databases/chembl/UniChem/data/oracleDumps")
