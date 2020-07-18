2aff2281c0aa0de4144b2b6d6d6d3b0e8de596d0
Paul Gaudin <paulgaudin314@gmail.com>
2020-07-17 17:37:20 -0700

    no remote_is_better()

--- dump.py
+++ dump.py
@@ -63,7 +63,7 @@ class Unichem_biothings_sdkDumper(FTPDumper):
         # self.client.login()
         for fn in ["UC_SOURCE.txt.gz"]:
             local_file = os.path.join(self.new_data_folder,fn)
-            if force or not os.path.exists(local_file) or self.remote_is_better(fn,local_file) or self.new_release_available():
+            if force or not os.path.exists(local_file) or self.new_release_available():
             	# path =  "ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/oracleDumps/" + self.release + "/" + fn
             	# self.logger.debug("PATHHHH")
             	# self.logger.debug(path)
